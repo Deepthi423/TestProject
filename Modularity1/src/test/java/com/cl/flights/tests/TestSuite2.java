10c4c6468c6a9c260734a306e957a82e05007e71
SriBog <Sri@BigDatay.com>
2019-05-17 17:55:34 +0100

    added 2 files

---
+++ Modularity1/src/test/java/com/cl/flights/tests/TestSuite2.java
@@ -0,0 +1,39 @@
+package com.cl.flights.tests;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+
+import com.cl.utils.AppUtils;
+import com.cl.utils.BrowserUtils;
+import com.cl.utils.ConstantUtils;
+
+public class TestSuite2 {
+	public WebDriver driver;
+	public static Properties prop;
+
+	@Before
+	public void setUp() throws Exception, IOException {
+		prop = new Properties();
+		prop.load(new FileInputStream(ConstantUtils.configPath));
+		driver = BrowserUtils.intailizeBrowser(prop, driver);
+		AppUtils.invokeApplication(prop, driver);
+	}
+
+	@After
+	public void tearDown() {
+		BrowserUtils.closeAppAndBrowser(driver);
+	}
+
+	@Test
+	public void test1() throws Exception {
+
+	}
+
+}
